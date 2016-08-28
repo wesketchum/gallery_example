f887c9893f4f180745df4c5f563daab9bc4f4d67
Wesley Ketchum <wketchum@fnal.gov>
2016-08-28 14:25:48 +0200

    a simple ReadEvent macro

---
+++ macros/demo_ReadEvent.C
@@ -0,0 +1,67 @@
+//some standard C++ includes
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+//some ROOT includes
+#include "TInterpreter.h"
+#include "TROOT.h"
+#include "TH1F.h"
+#include "TFile.h"
+
+//"art" includes (canvas, and gallery)
+#include "canvas/Utilities/InputTag.h"
+#include "gallery/Event.h"
+#include "gallery/ValidHandle.h"
+#include "canvas/Persistency/Common/FindMany.h"
+#include "canvas/Persistency/Common/FindOne.h"
+
+//convenient for us! let's not bother with art and std namespaces!
+using namespace art;
+using namespace std;
+
+void demo_ReadEvent() {
+
+  //By default, Wes hates the stats box! But by default, Wes forgets to disable it in his ROOT profile stuff...
+  gStyle->SetOptStat(0);
+
+  //Let's make a histogram to store event numbers.
+  //I ran this before, so I know my event range. You can adjust this for your file!
+  TH1F* h_events = new TH1F("h_events","Event Numbers;event;N_{events} / bin",100,4500,5000); 
+  
+  //We specify our files in a list of file names!
+  //Note: multiple files allowed. Just separate by comma.
+  vector<string> filenames { "MyInputFile_1.root" };
+
+  //I like having a timer ... that's just me.
+  TStopwatch timer;
+
+  //ok, now for the event loop! Here's how it works.
+  //
+  //gallery has these built-in iterator things.
+  //
+  //You declare an event with a list of file names. Then, you
+  //move to the next event by using the "next()" function.
+  //Do that until you are "atEnd()".
+  //
+  //In a for loop, that looks like this:
+
+  for (gallery::Event ev(filenames) ; !ev.atEnd(); ev.next()) {
+
+    //to get run and event info, you use this "eventAuxillary()" object.
+    cout << "Processing "
+	 << "Run " << ev.eventAuxiliary().run() << ", "
+	 << "Event " << ev.eventAuxiliary().event() << endl;
+
+    //ok, then we can fill our histogram!
+    h_events->Fill(ev.eventAuxiliary().event());
+
+  } //end loop over events!
+
+
+  //now, we're in a macro: we can just draw the histogram!
+  h_events->Draw();
+
+  //and ... done!
+}
